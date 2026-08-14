660624135b6e57c15e41a6883918fe0d09d53ec2
aneesh-p22 <aneeshpyaraka22@gmail.com>
2026-08-14 01:21:30 +0100

    defined initial GCN hyperparameter search space

--- configs/gcn_mutag.py
+++ configs/gcn_mutag.py
@@ -16,5 +16,10 @@ CONFIG = {
     "learning_rate": 0.001,
     "epochs": 100,
     "split_seeds": [0, 1, 2, 3, 4],
-    "training_seeds": [0, 1, 2, 3, 4]
+    "training_seeds": [0, 1, 2, 3, 4],
+
+    "search_space": {
+        "hidden_dim": [32, 64],
+        "learning_rate": [0.001, 0.0005]
+    }
 }
